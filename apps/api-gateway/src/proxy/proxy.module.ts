3261fe9e27f404ed37a56c365eac1e3d6efdc9fa
rubemar jr <rubemarrocha22@gmail.com>
2025-11-15 20:51:27 -0300

    fix(api-gateway): added controllers to proxy module context

--- apps/api-gateway/src/proxy/proxy.module.ts
+++ apps/api-gateway/src/proxy/proxy.module.ts
@@ -3,9 +3,27 @@ import { CoursesProxyController } from './controllers/courses-proxy.controller';
 import { CoursesProxyService } from './providers/courses-proxy.service';
 import { AuthProxyController } from './controllers/auth-proxy.controller';
 import { AuthProxyService } from './providers/auth-proxy.service';
+import { UserProxyController } from './controllers/user-proxy.controller';
+import { PaymentProxyController } from './controllers/payment-proxy.controller';
+import { VideoProxyController } from './controllers/video-proxy.controller';
+import { UserProxyService } from './providers/user-proxy.service';
+import { PaymentProxyService } from './providers/payment-proxy.service';
+import { VideoProxyService } from './providers/video-proxy.service';
 
 @Module({
-  controllers: [CoursesProxyController, AuthProxyController],
-  providers: [CoursesProxyService, AuthProxyService],
+  controllers: [
+    CoursesProxyController,
+    AuthProxyController,
+    UserProxyController,
+    PaymentProxyController,
+    VideoProxyController,
+  ],
+  providers: [
+    CoursesProxyService,
+    AuthProxyService,
+    UserProxyService,
+    PaymentProxyService,
+    VideoProxyService,
+  ],
 })
 export class ProxyModule {}
